b570b22244da480cd3260d0183c0861ef0bf5eca
Chris Kratz <Ckratz17@gmail.com>
2022-07-02 00:22:40 -0400

    redone according to the mock up

--- Assets/script.js
+++ Assets/script.js
@@ -1,74 +1,42 @@
-//start with prompt
 var wins = 0;
 var losses = 0;
 var ties = 0;
-var rock = "R" || "r"
-var paper = "P" || "p"
-var scissors = "S" || "s"
+var options = ['R', 'P', 'S'];  
 
-
-
-
-RPS()
-Results()
-    //userinput enter and click
-    //save user input in a variable and make sure to convert it into upper case
 function RPS() {
-    var ai = "RPS"
-    var aiPick = " "
-    var pick = ""
-    var user = prompt("Rock, Paper, Scissors", "R, P, or S")
+    var user = window.prompt("Rock, Paper, Scissors", "Enter R, P, or S")
     
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-      
-    if (randomNumber === 1) {
-        aiPick = rock
+    if (!user) {
+        return;
     }
-    if (randomNumber === 2) {
-        aiPick = paper
-    }
-    if (randomNumber === 3) {
-        aiPick = scissors
-    }
-       console.log(aiPick)
-}
+    user = user.toUpperCase
 
-function Results() {
-    pick = rock || paper || scissors
-    aiPick = rock || paper || scissors
-    if(pick === rock && aiPick === paper || 
-        pick === paper && aiPick === scissors || 
-        pick === scissors && aiPick === rock) {
-        alert("You lose! Your opponent choose " + aiPick)
-    }
-
-    if(pick === rock && aiPick === scissors || 
-        pick === paper && aiPick === rock || 
-        pick === scissors && aiPick === paper) {
-        alert("You win! Your opponent choose " + aiPick)
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
+    
+    if (user === computerChoice ) {
+        ties++
+        window.alert("Its a tie! Your opponent choose " + computerChoice)
+    }else if(
+        (user === 'R' && computerChoice === 'P') ||
+        (user === 'P' && computerChoice === 'S') ||
+        (user === 'S' && computerChoice === 'R')
+    ) {
+        losses++
+        window.alert("You Lose! Your opponent choose " + computerChoice)
+    }else {
+        wins++
+        window.alert("You Win! Your opponent choose " + computerChoice)
     }
 
-    if(pick === rock && aiPick === rock || 
-        pick === paper && aiPick === paper || 
-        pick === scissors && aiPick === scissors) {
-        alert("You tie! Your opponent choose " + aiPick)
-    }
-}
+    window.alert(
+        "Stats:\nWins: " + wins + "\nLosses: "+ losses + "\nTies: " + ties
+    )
+    
+    var playAgain = window.confirm("Play again?")
 
-//ai pick 
-    //create array = ["R", "S", "P"]
-    //pick random index of the array : math.floor(math.random()* 3)
-    //based on index capture the item(string)
-    //save computer pick in a variable
-
-//keep each win, loss, and tie in variables: 3 variables
-    //losses inital value 0, wins = 0, ties = 0;
-
-//compare user input with computer pick
-    //if(userinput === "R" && computerpick ==="P") || (userinput === "P" && computerpick ==="S") || (userinput === "S" && computerpick ==="R"))
-        //losses++;
-
-//show score on the popup(Alert)
-    //display score
-
-// to replay add confirm from user and run the same function agian.
+   if(playAgain) {
+    RPS()
+   }
+}   
+ RPS()
